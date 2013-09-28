b18cecdf36a39ed5cf9d99bf9540b67c24cdfd89
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-09-27 22:35:38 -0700

    Added setup new machine script.

---
+++ setup_new_ubuntu_machine.sh
@@ -0,0 +1,8 @@
+set -e
+# Install git.
+sudo apt-get install git
+# Install vim.
+sudo apt-get install vim
+# Install Google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
