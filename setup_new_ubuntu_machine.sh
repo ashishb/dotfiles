2529293f5505039043bb5d6f4495338f7ed285d0
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-09-29 02:28:18 -0700

    Update new ubuntu setup script.

--- setup_new_ubuntu_machine.sh
+++ setup_new_ubuntu_machine.sh
@@ -1,8 +1,14 @@
 set -e
-# Install git.
+# Install curl
+sudo apt-get install curl
+# Install dev tools.
+sudo apt-get install xsel
 sudo apt-get install git
-# Install vim.
 sudo apt-get install vim
+sudo apt-get install libmysqlclient-dev  # Needed for powerline.
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo easy_install powerline
 # Install Google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
