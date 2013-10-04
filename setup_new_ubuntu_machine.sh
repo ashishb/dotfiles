263afbe54a3b0c28f013b774c92933e15df94778
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-10-03 23:37:43 -0700

    Improved setup new ubuntu machine

--- setup_new_ubuntu_machine.sh
+++ setup_new_ubuntu_machine.sh
@@ -13,3 +13,7 @@ sudo easy_install powerline
 # Install Google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+# Via fixubuntu.com
+sudo apt-get remove -y unity-lens-shopping
+sudo sh -c 'echo "127.0.0.1 productsearch.ubuntu.com" >> /etc/hosts'
+gsettings set com.canonical.Unity.Lenses remote-content-search none
