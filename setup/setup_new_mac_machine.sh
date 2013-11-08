8e736c0d29c1e52d770ee88b74c09be828a6bc25
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-11-08 01:42:56 -0800

    Added setup script for Mac, moved setup scripts to setup dir.

---
+++ setup/setup_new_mac_machine.sh
@@ -0,0 +1,7 @@
+set -e
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+brew install wget
+brew install imagemagick
+# .gitignore boilerplate code (example: "gibo python textmate").
+brew install gibo
+brew install ctags
