c90416445e7dd22b4f27ecbe6ce8721235d09692
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-12-16 00:52:20 -0800

    Added some more packages to brew cask.

--- setup/setup_new_mac_machine.sh
+++ setup/setup_new_mac_machine.sh
@@ -5,3 +5,8 @@ brew install imagemagick
 # .gitignore boilerplate code (example: "gibo python textmate").
 brew install gibo
 brew install ctags
+# brew-cask converts brew into a package manager for mac packages.
+brew install brew-cask
+brew cask install google-chrom
+brew cask install xqartz  # For running X server based apps on Mac.
+brew cask install wireshark
