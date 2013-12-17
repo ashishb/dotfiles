e41257ea409a2123592282bf8d2f818a9ca64aeb
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-12-17 12:17:17 -0800

    Added some more brew packages.

--- setup/setup_new_mac_machine.sh
+++ setup/setup_new_mac_machine.sh
@@ -1,10 +1,21 @@
 set -e
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install latest version of Bash.
+brew install bash
 brew install wget
 brew install imagemagick
 # .gitignore boilerplate code (example: "gibo python textmate").
 brew install gibo
 brew install ctags
+brew install hexedit
+# Allows generation from notification from command line.
+brew install terminal-notifier
 # brew-cask converts brew into a package manager for mac packages.
 brew install brew-cask
 brew cask install google-chrom
