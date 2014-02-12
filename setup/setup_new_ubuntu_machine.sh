6adb9b5ae83d1a0466e23c54b7535c25580ea4f7
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2014-02-11 18:04:16 -0800

    Minor improvements to setup script for ubuntu.

--- setup/setup_new_ubuntu_machine.sh
+++ setup/setup_new_ubuntu_machine.sh
@@ -1,17 +1,17 @@
 set -e
 # Install curl
-sudo apt-get install curl
+sudo apt-get -y install curl
 # Install dev tools.
-sudo apt-get install xsel
-sudo apt-get install git
-sudo apt-get install whois
-sudo apt-get install vim
-sudo apt-get install python-dev
-sudo apt-get install python-pip
-sudo apt-get install ant
-sudo apt-get install nmap
-sudo apt-get remove thunderbird  # I don't need thunderbird.
-sudo apt-get install zip  # I am surprised, how this can be missing.
+sudo apt-get -y install xsel
+sudo apt-get -y install git
+sudo apt-get -y install whois
+sudo apt-get -y install vim
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
+sudo apt-get -y install ant
+sudo apt-get -y install nmap
+sudo apt-get -y remove thunderbird  # I don't need thunderbird.
+sudo apt-get -y install zip  # I am surprised, how this can be missing.
 # sudo apt-get install libmysqlclient-dev  # Needed for powerline.
 # sudo pip install powerline
 # Use pip instead of easy_install.
@@ -20,6 +20,7 @@ sudo pip install pylint
 sudo pip install Pygments
 sudo pip install pdbpp  # A powerful improvement to pdb CLI.
 # Install Google chrome
+sudo apt-get -y install libxss1  # Needed by Google-chrome.
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
