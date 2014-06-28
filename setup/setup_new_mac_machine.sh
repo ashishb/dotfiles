103209859aee1d9e80a66f27c3d9cd676098edc9
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2014-06-27 17:45:47 -0700

    Rearrange brew packages for Mac setup.

--- setup/setup_new_mac_machine.sh
+++ setup/setup_new_mac_machine.sh
@@ -8,24 +8,24 @@ sudo pip install pdbpp  # A powerful improvement to pdb CLI.
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 brew install ack  # A replacement for grep.
-# Install GNU core utilities (those that come with OS X are outdated)
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
 # Install latest version of Bash.
 brew install bash
 brew install bash-completion
-brew install wget
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+brew install ctags
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# .gitignore boilerplate code (example: "gibo python textmate").
+brew install gibo
+brew install hexedit
+brew install imagemagick
 brew install ssh-copy-id  # Easy way to set up key based login.
 # Install sshpass (unofficial since homebrew admins won't allow this formula in
 # the official repo).
 brew install https://raw.github.com/eugeneoden/homebrew/eca9de1/Library/Formula/sshpass.rb
-brew install imagemagick
-# .gitignore boilerplate code (example: "gibo python textmate").
-brew install gibo
-brew install ctags
-brew install hexedit
+brew install wget
 # Fix:
 # https://stackoverflow.com/questions/19215590/why-cant-i-install-any-gems-on-my-mac
 brew tap raggi/ale && brew install openssl-osx-ca
