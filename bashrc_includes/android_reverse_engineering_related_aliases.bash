2161a55e9013c02c18114e50b0adcaecf48c6df6
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2014-06-30 15:56:05 -0700

    Superfluous aliases in case of OS X - removed.

--- bashrc_includes/android_reverse_engineering_related_aliases.bash
+++ bashrc_includes/android_reverse_engineering_related_aliases.bash
@@ -16,16 +16,16 @@ else
   export ANDROID_HOME=$HOME/android_sdk
   # On mac, these are installed via homebrew.
   # TODO(ashishb): On GNU/Linux, install it using some package manager as well.
+  alias apktool="java -jar $HOME/tools/android/apktool.jar"
+  alias dex2jar=$HOME/tools/android/dex2jar-0.0.9.15/d2j-dex2jar.sh
   alias jd-gui="$HOME/AndroidTools/jd-gui/jd-gui"
 fi
 
 export ANDROID_SDK_ROOT=$ANDROID_HOME
-alias apktool="java -jar $HOME/tools/android/apktool.jar"
 alias burp="java -jar $HOME/Tools/burpsuite_free_v1.5.jar"
 alias printcert="keytool -printcert -file"
 alias signapk="jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore alias_name"
 alias android_screenshot="java -jar $HOME/tools/android/AndroidScreenCapture_1.1/AShot-1.1.jar"
-alias dex2jar=$HOME/tools/android/dex2jar-0.0.9.15/d2j-dex2jar.sh
 alias get_android_id='adb shell content query --uri content://settings/secure --projection name:value | grep android_id'
 function update_android_id(){
   # Update the android id.
