efea63350c069aa97d8d0d93d17ca552e34d84bf
Ashish Bhatia <ashishb@ashishb.net>
2018-10-22 18:38:00 -0700

    Add setup script for Cryptocurrencies related packages

---
+++ setup/setup_cryptocurrencies.sh
@@ -0,0 +1,16 @@
+# Google's Go Language
+brew install golang
+# Parity (Ethereum client) is written in this
+brew install cmake rust
+# Truffle for testing and deploying Solidity contracts
+npm install -g truffle
+# Web3 for Ethereum
+npm install -g web3
+# Solidity linter
+npm install -g solhint
+# Use Type script in lieu of Javascript for code maintainability
+npm install -g typescript
+
+# brew install kubernetes-cli
+# brew install kubernetes-helm
+
