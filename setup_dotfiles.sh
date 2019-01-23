25c755970310749569a43b53779abafcddcd7ed2
Ashish Bhatia <ashishb@ashishb.net>
2019-01-23 10:28:57 -0800

    Don't fail if softlinks cannot be created

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -26,7 +26,7 @@ echo "source $DIR/_vimrc" > $HOME/.vimrc
 
 # Not working.
 echo "Setting $HOME/.vim to link to $DIR/_vim directory"
-ln -s $DIR/_vim $HOME/.vim
+ln -s $DIR/_vim $HOME/.vim || true
 
 echo "Overwriting $HOME/.screenrc"
 echo "source $DIR/_screenrc" > $HOME/.screenrc
@@ -34,10 +34,10 @@ echo "source $DIR/_screenrc" > $HOME/.screenrc
 echo "Overwriting $HOME/.ssh/config"
 touch $DIR/_sshconfig
 mkdir $HOME/.ssh || true
-ln -s $DIR/_sshconfig ~/.ssh/config
+ln -s $DIR/_sshconfig ~/.ssh/config || true
 
 echo "Overwriting $HOME/.inpurtc"
-ln -s $DIR/_inputrc ~/.inputrc
+ln -s $DIR/_inputrc ~/.inputrc || true
 
 echo "Overwriting $HOME/.gitconfig"
 echo -e "[include]\n  path = $DIR/_gitconfig" > $HOME/.gitconfig
@@ -48,7 +48,7 @@ echo "%include $DIR/_hgrc" >> $HOME/.hgrc
 FILE="$HOME/.gradle/gradle.properties"
 echo "Overwriting $FILE"
 mkdir $HOME/.gradle || true
-ln -s $DIR/gradle.properties $FILE
+ln -s $DIR/gradle.properties $FILE || true
 
 
 echo "Configuring global gitignore file"
