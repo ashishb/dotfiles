b6e2348ece5757147fe8f91d4d017ac31856437e
Ashish Bhatia <ashishb@ashishb.net>
2020-04-20 23:33:06 -0700

    Test on Ubuntu using GitHub Actions (#41)

--- setup/setup_new_ubuntu_machine.sh
+++ setup/setup_new_ubuntu_machine.sh
@@ -8,8 +8,6 @@ sudo apt-get -y install curl
 sudo apt-get -y install git
 sudo apt-get -y install libxss1  # Required for Google-chrome.
 sudo apt-get -y install nmap
-# http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
-sudo apt-get -y install oracle-java8-installer  # Required for android.
 sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
 sudo apt-get -y install ssh
@@ -17,6 +15,12 @@ sudo apt-get -y install vim
 sudo apt-get -y install whois
 sudo apt-get -y install xsel
 sudo apt-get -y install zip  # I am surprised, how this can be missing.
+# For some reason, this fails now
+# We don't need Java 8 on Linux right now anyways as I don't use Linux for
+# Android development
+# http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
+# sudo apt-get -y install oracle-java8-installer  # Required for android.
+
 sudo apt-get -y remove thunderbird  # I don't need thunderbird.
 sudo pip install --upgrade pip
 # Use pip instead of easy_install.
@@ -24,13 +28,7 @@ sudo pip install --upgrade pip
 sudo pip install pylint
 sudo pip install Pygments
 sudo pip install pdbpp  # A powerful improvement to pdb CLI.
-# Install Google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-if test "${CI}"; then
-    # https://askubuntu.com/questions/1065231/dpkg-deb-error-archive-has-premature-member-control-tar-xz-before-contr/1100361
-    sudo apt-get install dpkg
-fi
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install google-chrome-stable
 
 #### Ubuntu specific settings. ####
 # Via fixubuntu.com
