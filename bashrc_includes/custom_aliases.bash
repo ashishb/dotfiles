5b4724f650941b264b173c5c6008ee56a0ff8531
Ashish Bhatia <ashishb@ashishb.net>
2020-12-19 00:29:06 +0530

    Add a Bluetooth force restart function

--- bashrc_includes/custom_aliases.bash
+++ bashrc_includes/custom_aliases.bash
@@ -64,6 +64,14 @@ function ghpr()
   open "${url}"
 }
 
+# Mac OS only
+function restart_bluetooth()
+{
+  # Source: https://gist.github.com/nicolasembleton/afc19940da26716f8e90
+  sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+  sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
+}
+
 
 alias myghpr="gh pr --me --link"
 alias myghissues="open https://github.com/issues/assigned"
