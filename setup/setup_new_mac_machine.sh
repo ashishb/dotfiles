dd405620b80a7c2cf5c6873d2b3430adee8bce13
Ashish Bhatia <ashishb@ashishb.net>
2023-04-09 16:41:54 -0700

    Add blueutil - Bluetooth CLI

--- setup/setup_new_mac_machine.sh
+++ setup/setup_new_mac_machine.sh
@@ -194,6 +194,10 @@ brew install jd-gui # For java decompilation.
 # Android emulator is good enough now, therefore, I won't be using genymotion anymore.
 # brew install virtualbox  # Needed for GenyMotion.
 # brew install genymotion  # Emulator for android.
+
+# Bluetooth CLI for mac
+brew install blueutil
+
 # This one seems to fail on GitHub Actions
 # https://github.com/ashishb/dotfiles/runs/2258896886
 # brew cleanup || true
