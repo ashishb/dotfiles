24c0e0243718848caf661a2d7848aa40a16af6e2
Ashish Bhatia <ashishb@ashishb.net>
2026-06-29 21:59:28 -0700

    Fix typos and update comments in setup script (#83)

--- setup/setup_new_ubuntu_machine.sh
+++ setup/setup_new_ubuntu_machine.sh
@@ -21,30 +21,26 @@ sudo apt-get -y install ant \
   ripgrep \
   golang-go
 
-# Install latest version of Go - https://github.com/golang/go/wiki/Ubuntu
-# sudo add-apt-repository -y ppa:longsleep/golang-backports && sudo apt update && sudo apt -y install golang-go
-# Install Python 3.9
-sudo add-apt-repository -y ppa:deadsnakes/ppa && sudo apt -y install python3.9 python3-pip
+# Install the latest version of Go - https://github.com/golang/go/wiki/Ubuntu
 
 # For some reason, this fails now
-# We don't need Java 8 on Linux right now anyways as I don't use Linux for
+# We don't need Java 8 on Linux right now, anyway, as I don't use Linux for
 # Android development
 # http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
 # sudo apt-get -y install oracle-java8-installer  # Required for android.
 
-sudo apt-get -y remove thunderbird  # I don't need thunderbird.
-sudo pip install --upgrade pip
+sudo apt-get -y remove thunderbird  # I don't need Thunderbird.
 # Use pip instead of easy_install.
 # http://stackoverflow.com/questions/3220404/why-use-pip-over-easy-install
 sudo apt-get install google-chrome-stable
 
-#### Ubuntu specific settings. ####
+#### Ubuntu-specific settings. ####
 # Via fixubuntu.com
 sudo apt-get remove -y unity-lens-shopping
 sudo sh -c 'echo "127.0.0.1 productsearch.ubuntu.com" >> /etc/hosts'
 ## Save gnome session on exit.
 #command -v dconf 1>/dev/null &&
 # dconf write /org/gnome/gnome-session/auto-save-session true 1>/dev/null &&
-## Don't sent Unity length results to Amazon.
+## Don't send Unity length results to Amazon.
 #command -v gsettings 1>/dev/null &&
 # 	gsettings set com.canonical.Unity.Lenses remote-content-search none
